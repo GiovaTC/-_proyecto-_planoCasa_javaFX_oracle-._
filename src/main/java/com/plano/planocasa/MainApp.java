a51d37c8c0527f6f74ca39eb767fa37e4b5dd918
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-17 14:44:55 -0500

    commit-MainApp :.

--- src/main/java/com/plano/planocasa/MainApp.java
+++ src/main/java/com/plano/planocasa/MainApp.java
@@ -1,19 +1,31 @@
 package com.plano.planocasa;
 
+import com.plano.planocasa.dao.HouseDAO;
+import com.plano.planocasa.dao.RoomDAO;
+import com.plano.planocasa.model.House;
+import com.plano.planocasa.model.Room;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class MainApp extends Application {
+    private Canvas canvas;
+    private GraphicsContext gc;
+    private House currentHouse;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        //
     }
 }
