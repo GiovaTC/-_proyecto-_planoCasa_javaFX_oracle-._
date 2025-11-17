6fb87dd40e622d12915b73e21c3d17115f770b32
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-17 13:54:36 -0500

    commit - DBUtil .

---
+++ src/main/java/com/plano/planocasa/DBUtil.java
@@ -0,0 +1,7 @@
+package com.plano.planocasa;
+
+import java.sql.Connection;
+
+
+public class DBUtil {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.plano.planocasa {
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.plano.planocasa to javafx.fxml;
     exports com.plano.planocasa;
