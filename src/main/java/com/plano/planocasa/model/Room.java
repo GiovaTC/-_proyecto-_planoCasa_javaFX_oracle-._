5cd062fb089caf51d2c0552f67d5962e9fd50bfe
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-17 14:09:25 -0500

    commit-Room .. . :.

--- src/main/java/com/plano/planocasa/model/Room.java
+++ src/main/java/com/plano/planocasa/model/Room.java
@@ -1,4 +1,87 @@
 package com.plano.planocasa.model;
 
 public class Room {
+    private Long id;
+    private Long houseId;
+    private String name;
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+    private double area;
+
+    public Room() {}
+
+    public Room(String name, double x, double y, double width, double height) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.area = width * height / 100.0;
+    }
+
+    public Long getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(Long houseId) {
+        this.houseId = houseId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
 }
